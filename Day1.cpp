7db0193d1183d59f2425943b335b96af1f093d27
Podkovenko Yaroslav <yarienotfound@gmail.com>
2024-02-06 22:49:29 +0100

    Bit Counting(Day1) edit
    Some of variables were out of use

--- Day1.cpp
+++ Day1.cpp
@@ -1,11 +1,9 @@
 unsigned int countBits(unsigned long long n){
-    int bin = 0, k = 1, result = 0, now;
+    int result = 0, now;
     while(n)
     {
       now = n % 2;
       if(now)result++;
-      bin += now * k;
-      k *= 10;
       n /= 2;
     }
   return result;
